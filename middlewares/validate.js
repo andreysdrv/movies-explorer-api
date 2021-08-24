7bf08f4396924ee329d82fcdb55621d9e71310aa
andreysdrv <jeck666@mail.ru>
2021-08-24 19:09:46 +0300

    feat: добавить валидацию на роуты фильмов

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,3 +1,29 @@
 const { celebrate, Joi } = require('celebrate');
 
 const linkRegExp = /(http:\/\/|https:\/\/)(www)*[a-z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+#*/;
+
+const idValidation = celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().alphanum().length(24).hex(),
+  }),
+});
+
+const movieValidation = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(linkRegExp),
+    trailer: Joi.string().required().pattern(linkRegExp),
+    thumbnail: Joi.string().required().pattern(linkRegExp),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required()
+  }),
+});
+
+module.exports = {
+  idValidation,
+  movieValidation
+}
--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,11 @@
 const { Router } = require('express')
 const { getMovies, createMovie, removeMovie } = require('../controllers/movies')
+const { idValidation, movieValidation } = require('../middlewares/validate')
 
 const moviesRouter = Router()
 
 moviesRouter.get('/movies', getMovies)
-moviesRouter.post('/movies', createMovie)
-moviesRouter.delete('/movies/:_id', removeMovie)
+moviesRouter.post('/movies', movieValidation, createMovie)
+moviesRouter.delete('/movies/:_id', idValidation, removeMovie)
 
 module.exports = moviesRouter
